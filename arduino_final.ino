9f9c0a769c8dfb307ebb5b9cdbdbb184d6ae11e5
Shruti Hiray <shrutihiray15@gmail.com>
2015-06-11 14:32:48 +0530

    Added arduino code

---
+++ arduino_final.ino
@@ -0,0 +1,40 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial bluetooth(10, 11); // RX, TX //declaration of receiving pin and transmitting pin of serial data of bluetooth
+int buzzer = 8;// defined pin where buzzer is attached
+int BluetoothData;
+
+void setup() {
+  // put your setup code here, to run once:
+bluetooth.begin(9600);// 9600 is baud rate
+  pinMode(buzzer, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (bluetooth.available())// if data is available 
+  {
+    BluetoothData = bluetooth.read();
+    
+
+    if (BluetoothData == '1')
+    {
+      digitalWrite(buzzer, 1);// if received data is '1' make buzzer pin high
+      
+      Serial.println("BUZZER ON");
+    }
+
+    if (BluetoothData == '0')
+    {
+      digitalWrite(buzzer, 0);// if received data is '1' make buzzer pin low
+      
+      Serial.println("BUZZER OFF");
+    }
+
+
+
+  }
+  delay(100);
+}
